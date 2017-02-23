7bcfd8a5d8b2d27a6de3ff319473f44be646fc65
Spencer Seeger <seeger.spencer@gmail.com>
2017-02-23 10:31:35 -0700

    IDK

--- app.js
+++ app.js
@@ -1,72 +1,69 @@
-angular.module('News', ['ui.router'])
- .factory('postFactory', [function(){
+var module = angular.module('News', ['ui.router']);
+module.factory('postFactory', [function () {
     var o = {
-      posts: []
+        posts: []
     };
     return o;
-  }])
-  .config([
+}]);
+
+module.config([
     '$stateProvider',
     '$urlRouterProvider',
-    function($stateProvider, $urlRouterProvider) {
-      $stateProvider
-        .state('home', {
-          url: '/home',
-          templateUrl: 'index.html',
-          controller: 'MainCtrl'
-        });
+    function ($stateProvider, $urlRouterProvider) {
         $stateProvider
-        .state('posts',{
-             url: '/posts/{id}',
-            templateUrl: 'posts.html',
+            .state('home', {
+                url: '/home',
+                templateUrl: '/home.html',
+                controller: 'MainCtrl'
+            });
+        $stateProvider.state('posts', {
+            url: '/posts/{id}',
+            templateUrl: '/posts.html',
             controller: 'PostCtrl'
         });
-      $urlRouterProvider.otherwise('home');
-  }])
-.controller('MainCtrl', [
-    '$scope',
-    'postFactory',
-    function($scope,postFactory){
+        $urlRouterProvider.otherwise('home');
+    }]);
 
-        $scope.posts = postFactory.posts;
-
-        $scope.addPost = function() {
-            $scope.posts.push({title:$scope.formContent,upvotes:0});
-            $scope.formContent='';
-        };
-
-        $scope.incrementUpvotes = function(post) {
-            post.upvotes += 1;
-
-        $scope.addPost = function(){
-      if($scope.formContent === '') { return; }
-      $scope.posts.push({
-        title: $scope.formContent,
-        upvotes: 0,
-        comments: []
-      });
-      $scope.title = '';
-    };
-
-        };
-    }
-])
-.controller('PostCtrl', [
+module.controller('PostCtrl', [
     '$scope',
     '$stateParams',
-    'postFactory', 
-    function($scope, $stateParams, postFactory){
-      $scope.post = postFactory.posts[$stateParams.id];
-      $scope.addComment = function(){
-        if($scope.body === '') { return; }
-        $scope.post.comments.push({
-          body: $scope.body,
-          upvotes: 0
-        });
-        $scope.body = '';
-      };
-    $scope.incrementUpvotes = function(comment){
-      comment.upvotes += 1; 
-    };
-  }]);
-  
+    'postFactory',
+    function ($scope, $stateParams, postFactory) {
+        $scope.post = postFactory.posts[$stateParams.id];
+        $scope.addComment = function () {
+            if ($scope.body === '') { return; }
+            $scope.post.comments.push({
+                body: $scope.body,
+                upvotes: 0
+            });
+            $scope.body = '';
+        };
+        $scope.incrementUpvotes = function (comment) {
+            comment.upvotes += 1;
+        };
+    }]);
+
+module.controller('MainCtrl', [
+    '$scope',
+    'postFactory',
+    function ($scope, postFactory) {
+        $scope.posts = postFactory.posts;
+        $scope.addPost = function () {
+            $scope.posts.push({ title: $scope.formContent, upvotes: 0 });
+            $scope.formContent = '';
+            $scope.incrementUpvotes = function (post) {
+                post.upvotes += 1;
+            };
+        };
+        $scope.addPost = function () {
+            if ($scope.formContent === '') { return; }
+            $scope.posts.push({
+                title: $scope.formContent,
+                upvotes: 0,
+                comments: []
+            });
+            $scope.title = '';
+        };
+    }
+]);
+
