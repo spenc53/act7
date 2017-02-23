e427d3dc536bb9f0a3a2fc3071194862b8cd57a5
Spencer Seeger <seeger.spencer@gmail.com>
2017-02-23 10:11:29 -0700

    asdf

---
+++ app.js
@@ -0,0 +1,61 @@
+angular.module('News', ['ui.router'])
+ .factory('postFactory', [function(){
+    var o = {
+      posts: []
+    };
+    return o;
+  }])
+  .config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+      $stateProvider
+        .state('home', {
+          url: '/home',
+          templateUrl: 'index.html',
+          controller: 'MainCtrl'
+        });
+        $stateProvider
+        .state('posts',{
+             url: '/posts/{id}',
+            templateUrl: 'posts.html',
+            controller: 'PostCtrl'
+        });
+      $urlRouterProvider.otherwise('home');
+  }])
+.controller('MainCtrl', [
+    '$scope',
+    'postFactory',
+    function($scope,postFactory){
+
+        $scope.posts = postFactory.posts;
+
+        $scope.addPost = function() {
+            $scope.posts.push({title:$scope.formContent,upvotes:0});
+            $scope.formContent='';
+        };
+
+        $scope.incrementUpvotes = function(post) {
+            post.upvotes += 1;
+        };
+    }
+])
+.controller('PostCtrl', [
+    '$scope',
+    '$stateParams',
+    'postFactory', 
+    function($scope, $stateParams, postFactory){
+      $scope.post = postFactory.posts[$stateParams.id];
+      $scope.addComment = function(){
+        if($scope.body === '') { return; }
+        $scope.post.comments.push({
+          body: $scope.body,
+          upvotes: 0
+        });
+        $scope.body = '';
+      };
+    $scope.incrementUpvotes = function(comment){
+      comment.upvotes += 1; 
+    };
+  }]);
+  
